d86f8afd763130bf67bdf8d86533fd553d06176a
vannia01yohanna <vannia01yohanna@gmail.com>
2025-12-04 02:49:06 -0600

    MODULO66

---
+++ components/products/ProductSearchForm.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { SearchSchema } from "@/src/schema"
+import { toast } from "react-toastify"
+import { redirect } from "next/navigation"
+import { useRouter } from "next/navigation"
+
+export default fuction ProductSearchForm (){
+    const router = useRouter()
+
+
+const handleSearchForm = (formData: FormData) => {
+    const data ={
+        search: formData.get('search')
+    }
+    const result = SearchSchema.safeParse(data)
+    if (!result.seccess){
+        result.error.issues.forEach(issue => {
+            toast.error(issue.massage)
+        })
+        return
+    }
+    router.push ('/admin/products/search?search=${result.data.search}')
+
+
+}
+    return (
+        <form className="flex item-center">
+        <input 
+        type="text"
+        placeholder="Buscar Producto"
+        className="p-2 placeholder-gray-400 w-full"
+        name="search"
+        />
+
+        <input 
+        type="submit"
+        className="bg-indigo-600 p-2 uppercase text-white cursor-pointer"
+        value={'Buscar'}
+        />
+
+        </form>
+    )
+}
